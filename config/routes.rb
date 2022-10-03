6717043bb34b6bb39e96aa800f8f37e8d299f337
franklin duque <franklinmont@gmail.com>
2022-10-02 21:29:06 -0500

    version 2 funcionando en heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  devise_for :users
+ 
+  root to: "cocktails#index"
+  resources :cocktails, only: %i[index show new create destroy] do
+    resources :bookings, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :bookings, only: %i[destroy show]
 end
