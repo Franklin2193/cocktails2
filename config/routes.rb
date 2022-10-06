5396ff39966a2b8d80f6af2ddeaaedab16d4318a
Esteban Lopez <estebanlopez_99@hotmail.com>
2022-10-05 19:03:15 -0500

    [ADD] edit, destroy functionality according to role, destroy is still not working

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   resources :users, only: [:show] # get 'users/:id' => 'users#show'
 
   root to: "cocktails#index"
-  resources :cocktails, only: %i[index show new create destroy] do
+  resources :cocktails, only: %i[index show new edit update create destroy] do
     resources :bookings, only: %i[new create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :bookings, only: %i[destroy show]
+  resources :bookings, only: %i[destroy show edit update]
 end
